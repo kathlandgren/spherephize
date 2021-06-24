8c03ad6ca9fa336f63c6f937c83fd527403fe593
kathlandgren <ek672@cornell.edu>
2021-06-24 17:43:13 -0600

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../spherephize'))
+sys.path.insert(0, os.path.abspath("../spherephize"))
 
 
 # -- Project information -----------------------------------------------------
